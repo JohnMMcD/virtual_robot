1d2d6a93cb721319f611da4aceb3f72681cc15e5
John McDonnell <john.mcdonnell@oracle.com>
2020-06-28 14:28:04 -0400

    Now reports encoder distance as "Undefined" if the sensor gives a too-high reading. Now can use buttons to control drive motors.

--- TeamCode/src/org/firstinspires/ftc/teamcode/ArmBotDemo.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/ArmBotDemo.java
@@ -16,7 +16,17 @@ import org.firstinspires.ftc.robotcore.external.navigation.*;
 @TeleOp(name = "arm bot demo", group = "ArmBot")
 public class ArmBotDemo extends LinearOpMode {
 
+    private String getDistance(DistanceSensor sensor)
+    {
+        String sTempDistance;
+        double dRawDistance = sensor.getDistance(DistanceUnit.CM);
+        sTempDistance = dRawDistance < 815 ?  String.format("%f", dRawDistance) : "Undefined";
+        return sTempDistance;
+    }
+
     public void runOpMode(){
+        telemetry.addData("Hardware mapping","begin");
+        telemetry.update();
         DcMotor m1 = hardwareMap.dcMotor.get("back_left_motor");
         DcMotor m2 = hardwareMap.dcMotor.get("front_left_motor");
         DcMotor m3 = hardwareMap.dcMotor.get("front_right_motor");
@@ -42,6 +52,8 @@ public class ArmBotDemo extends LinearOpMode {
         DistanceSensor leftDistance = hardwareMap.get(DistanceSensor.class, "left_distance");
         DistanceSensor rightDistance = hardwareMap.get(DistanceSensor.class, "right_distance");
         DistanceSensor backDistance = hardwareMap.get(DistanceSensor.class, "back_distance");
+        telemetry.addData("Hardware mapping","end");
+        telemetry.update();
         //gyro.init();
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -57,28 +69,98 @@ public class ArmBotDemo extends LinearOpMode {
 
         ColorSensor colorSensor = hardwareMap.colorSensor.get("color_sensor");
         telemetry.addData("Press Start Button When Ready","");
+        telemetry.addData("Front Distance", getDistance(frontDistance));
+        telemetry.addData("Left Distance", getDistance(leftDistance));
+        telemetry.addData("Right Distance", getDistance(rightDistance));
+        telemetry.addData("Back Distance", getDistance(backDistance));
+        telemetry.addData("Encoders"," %d %d %d %d", m1.getCurrentPosition(), m2.getCurrentPosition(),
+                m3.getCurrentPosition(), m4.getCurrentPosition());
+
+/*
+        telemetry.addData("Encoders"," %d %d %d %d", m1.getCurrentPosition(), m2.getCurrentPosition(),
+                m3.getCurrentPosition(), m4.getCurrentPosition());
+
+ */
         telemetry.update();
 
         gamepad1.setJoystickDeadzone(0.05f);
 
         waitForStart();
+        boolean bButtonWasPressed = false;
+
         while (opModeIsActive()){
-            double px = gamepad1.left_stick_x;
-            double py = -gamepad1.left_stick_y;
-            double pa = gamepad1.left_trigger - gamepad1.right_trigger;
-            if (Math.abs(pa) < 0.05) pa = 0;
-            double p1 = -px + py - pa;
-            double p2 = px + py + -pa;
-            double p3 = -px + py + pa;
-            double p4 = px + py + pa;
-            double max = Math.max(1.0, Math.abs(p1));
-            max = Math.max(max, Math.abs(p2));
-            max = Math.max(max, Math.abs(p3));
-            max = Math.max(max, Math.abs(p4));
-            p1 /= max;
-            p2 /= max;
-            p3 /= max;
-            p4 /= max;
+            double p1;
+            double p2;
+            double p3;
+            double p4;
+
+            if (bButtonWasPressed || gamepad1.x || gamepad1.y || gamepad1.a || gamepad1.b)
+            {
+                //             Buttons are arranged:
+                //
+                //                  Y
+                //               X     B
+                //                  A
+                //
+                // Buttons were pressed - use them and ignore the joystick
+                bButtonWasPressed = gamepad1.x || gamepad1.y || gamepad1.a || gamepad1.b;
+
+                if (gamepad1.y) // go forwards
+                {
+                    p1 = 1f;
+                    p2 = 1f;
+                    p3 = 1f;
+                    p4 = 1f;
+                }
+                else if (gamepad1.x) // strafe left
+                {
+                    p1 = 1f;
+                    p2 = -1f;
+                    p3 = 1f;
+                    p4 = -1f;
+                }
+                else if (gamepad1.a) // go backwards
+                {
+                    p1 = -1f;
+                    p2 = -1f;
+                    p3 = -1f;
+                    p4 = -1f;
+                }
+                else if (gamepad1.b) // strafe right
+                {
+                    p1 = -1f;
+                    p2 = 1f;
+                    p3 = -1f;
+                    p4 = 01f;
+                }
+                else // nothing pressed - stop
+                {
+                    p1 = 0f;
+                    p2 = 0f;
+                    p3 = 0f;
+                    p4 = 0f;
+                }
+            }
+            else
+            {
+                double px = gamepad1.left_stick_x;
+                double py = -gamepad1.left_stick_y;
+                double pa = gamepad1.left_trigger - gamepad1.right_trigger;
+                if (Math.abs(pa) < 0.05) pa = 0;
+                p1 = -px + py - pa;
+                p2 = px + py + -pa;
+                p3 = -px + py + pa;
+                p4 = px + py + pa;
+                double max = Math.max(1.0, Math.abs(p1));
+                max = Math.max(max, Math.abs(p2));
+                max = Math.max(max, Math.abs(p3));
+                max = Math.max(max, Math.abs(p4));
+                p1 /= max;
+                p2 /= max;
+                p3 /= max;
+                p4 /= max;
+            }
+
             m1.setPower(p1);
             m2.setPower(p2);
             m3.setPower(p3);
@@ -91,14 +173,15 @@ public class ArmBotDemo extends LinearOpMode {
             Orientation orientation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
             telemetry.addData("Heading", " %.1f", orientation.firstAngle * 180.0 / Math.PI);
 
-            telemetry.addData("Front Distance", " %.1f", frontDistance.getDistance(DistanceUnit.CM));
-            telemetry.addData("Left Distance", " %.1f", leftDistance.getDistance(DistanceUnit.CM));
-            telemetry.addData("Right Distance", " %.1f", rightDistance.getDistance(DistanceUnit.CM));
-            telemetry.addData("Back Distance", " %.1f", backDistance.getDistance(DistanceUnit.CM));
+            telemetry.addData("Front Distance", getDistance(frontDistance));
+            telemetry.addData("Left Distance", getDistance(leftDistance));
+            telemetry.addData("Right Distance", getDistance(rightDistance));
+            telemetry.addData("Back Distance", getDistance(backDistance));
             telemetry.addData("Encoders"," %d %d %d %d", m1.getCurrentPosition(), m2.getCurrentPosition(),
                     m3.getCurrentPosition(), m4.getCurrentPosition());
             telemetry.update();
         }
+        // Op mode is no longer active - stop the motors
         m1.setPower(0);
         m2.setPower(0);
         m3.setPower(0);
